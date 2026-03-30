29f5b918b79a47e2962b54ae173043f4b4b367af
Loïc Bertetto <loicbertetto@Host-002.lan>
2026-03-30 11:48:19 +0200

    fix: remotePatterns images

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,17 @@
-/** next.config.ts */
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   images: {
-    domains: [
-      'i.scdn.co',
-      'is1-ssl.mzstatic.com',
-      'e-cdns-images.dzcdn.net',
-      'lh3.googleusercontent.com',
+    unoptimized: true,
+    remotePatterns: [
+      { protocol: 'https', hostname: 'coverartarchive.org' },
+      { protocol: 'https', hostname: '*.coverartarchive.org' },
+      { protocol: 'https', hostname: 'i.scdn.co' },
+      { protocol: 'https', hostname: 'is1-ssl.mzstatic.com' },
+      { protocol: 'https', hostname: 'e-cdns-images.dzcdn.net' },
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
     ],
     formats: ['image/avif', 'image/webp'],
-  },
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "frame-src 'self' https://www.youtube.com https://www.youtube-nocookie.com;",
-          },
-        ],
-      },
-    ]
   },
 }
 
